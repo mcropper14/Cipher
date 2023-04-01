6b7a67c04ba632c7effd88323160d7e86a513675
mdreed19 <mdreed@wm.edu>
2023-03-31 22:13:00 -0400

    major + class + exams

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+from random import randint
 class Twamp:
 
     def __init__(self):
         self.happiness = 100
         self.twampness = 0 
-        self.d_dollars = 800 #maybe don't put here
+        self.d_dollars = 0
         self.health = 100
         self.stress = 0 
-        self.hygiene = 0 
-
+        self.hygiene = 0
+        self.schedule = 0
 
     def name(self):
         pass 
@@ -43,10 +44,24 @@ class Twamp:
         pass
 
     def major(self):
-        pass 
+        print('Majors Include: STEM or Not STEM')
+        maj = input()
+        print('Choose Your Fate Wisely: ' + maj)
+        if maj.lower() == 'stem':
+            self.stress += 20
+        if maj.lower() == 'not stem':
+            self.stress -= 20 
 
+    def classes(self):
+        c_num = input()
+        print('Enter Number of Classes Enrolled In: ' + c_num)
+        self.schedule = c_num
+    
     def exams(self):
-        pass 
+        grades = []
+        for i in range(self.schedule):
+            grades.append(randint(0, 100))
+        print('These are your exam grades for your ' + self.schedule + 'classes: ' + grades)
 
     def disaster(self, disaster):
         if self.d_dollars == 0:
