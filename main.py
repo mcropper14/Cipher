7551d845714a94d3d6410386e77f65e25cd019bd
Myra <myracropper32@gmail.com>
2023-03-31 20:22:22 -0400

    main

---
+++ main.py
@@ -0,0 +1,41 @@
+class Twamp:
+
+    def __init__(self):
+        self.happiness = 100
+        self.twampness = 0 
+        self.d_dollars = 800 #maybe don't put here
+        self.health = 100
+        self.stress = 0 
+        self.hygiene = 0 
+
+
+    def name(self):
+        pass 
+
+    def person_info(self):
+        pass 
+    
+    def display(self):
+        pass
+
+    def swem(self):
+        pass
+
+    def club(self):
+        pass
+
+    def greek_life(self):
+        pass
+
+    def orientation(self, days, socialize):
+        pass
+
+    def major(self):
+        pass 
+
+    def exams(self):
+        pass 
+
+    def disaster(self, disaster):
+        pass 
+
